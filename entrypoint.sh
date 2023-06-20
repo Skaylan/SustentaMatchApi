48c7a12e343a2c3a9dd81a0d6648acaf2c69f907
Lucas Gomes <lucas_7tv@hotmail.com>
2023-06-20 10:21:58 -0300

    change dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python3 -m pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+python3 -m pip install --no-cache-dir -r requirements.txt
 
 python3 -m flask db init
 python3 -m flask db upgrade
